494516e0bd3398a2f32a4cb190a183749d3a48ac
Gabriel Barreiro <contato@gabrielbarreiro.com>
2026-04-04 15:06:36 -0300

    FIX: Add description adjustment and adjusted signal

--- quit_poverty/parse/btg.py
+++ quit_poverty/parse/btg.py
@@ -8,7 +8,7 @@ import os
 import pandas as pd
 import msoffcrypto as msoff
 
-from ..misc import list_files
+from ..misc import adjust_description, list_files
 
 
 def remove_password(path: str, password: str) -> pd.DataFrame:
@@ -69,6 +69,8 @@ def parse_btg_account(path: str) -> pd.DataFrame:
         axis=1, func=lambda row: f"{row["transaction"]} - {row["original_description"]}"
     )
 
+    raw_data["description"] = raw_data["description"].apply(adjust_description)
+
     raw_data = raw_data.drop(
         columns=["transaction", "original_description", "category"]
     )
@@ -114,12 +116,14 @@ def parse_btg_credit_card(path: str) -> pd.DataFrame:
         raw_data = pd.concat([raw_data, df], ignore_index=True)
 
     raw_data = raw_data.dropna(subset=["description"])
+    raw_data["description"] = raw_data["description"].apply(adjust_description)
+
     raw_data = raw_data[raw_data["date_ref"] != "Data"]
 
     raw_data["date_ref"] = pd.to_datetime(
         raw_data["date_ref"].astype(str).str.split(" ").str[0], yearfirst=True
     )
-    raw_data["amount"] = raw_data["amount"].astype(float)
+    raw_data["amount"] = raw_data["amount"].astype(float) * -1
 
     raw_data["source"] = "cartao_btg"
 
